2d2d953d8bf0dce0294a2244fbd47e4ef81a3b66
MartinezSafari <martinez@thereachtrust.org>
2024-01-09 10:06:29 +0200

    Display the hard coded value of the game

--- GameShell-ios-app/ViewControllers/GameViewController.swift
+++ GameShell-ios-app/ViewControllers/GameViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet var sgDiff : UISegmentedControl!
+    @IBOutlet var lbDiff : UILabel!
+    @IBOutlet var lbLevel : UILabel!
+    @IBOutlet var lbScore : UILabel!
+    @IBOutlet var lbTime : UILabel!
+    
+    func updateDifficulty(){
+        let diff = sgDiff.selectedSegmentIndex
+        
+        if diff == 0{
+            lbDiff.text = "Difficulty: Easy"
+        }
+        else if diff == 1{
+            lbDiff.text = "difficulty: Medium"
+        }
+        else{
+            lbDiff.text = "Difficulty: Hard"
+        }
+    }
+    
+    @IBAction func segmentDidChange( sender : UISegmentedControl){
+        updateDifficulty()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        updateDifficulty()
+        
+        lbLevel.text = "Level: 5"
+        lbScore.text = "Score: 273"
+        lbTime.text = "Time Left: 50"
     }
     
 
